5ecda364b5de1defd2e7b5ecd911957677efc76a
nehri97 <doublerico7@gmail.com>
2015-04-12 22:06:48 -0400

    make it better 2

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'helloworld.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^/', include(app.urls))
+	url(r'^/', include(app.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
