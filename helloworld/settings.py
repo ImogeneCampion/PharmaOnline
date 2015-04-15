fd91aee4e719098e678b162854cc6d747725637a
nehri97 <doublerico7@gmail.com>
2015-04-15 11:17:25 -0400

    ###

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -98,10 +98,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
